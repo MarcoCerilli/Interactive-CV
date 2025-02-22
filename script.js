716f1ad979c1da091f540209d4554b5ec64514c3
Marco Cerilli <cerillimarco15@gmail.com>
2025-02-22 15:50:45 +0100

    Le mie modifiche locali prima del pull

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
 document.getElementById("darkModeBtn").addEventListener("click", function() {
-document.body.classList.toggle("dark-mode");
+    document.body.classList.toggle("dark-mode");
+    console.log(document.body.classList);  
 });
 
 document.getElementById("downloadCV").addEventListener("click", function() {
     const link = document.createElement("a");
-    link.href = "cv_marco_cerilli.pdf";
-    link.download = "CV_Marco_Cerilli.pdf";
+    link.href = "https://marcocerilli.github.io/Interactive-CV/";
+    link.download = "CV_Marco_Cerilli";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);

ab4c408fc1965783f3500b8f4dd8b941cba2672d
Marco Cerilli <cerillimarco15@gmail.com>
2025-02-22 16:16:54 +0100

    Primo commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById("darkModeBtn").addEventListener("click", function() {
 document.getElementById("downloadCV").addEventListener("click", function() {
     const link = document.createElement("a");
     link.href = "https://marcocerilli.github.io/Interactive-CV/CV_Marco_Cerilli.pdf";
-    link.download = "CV_Marco_Cerilli";
+    link.download = "CV_Marco_Cerilli.pdf.pdf";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);

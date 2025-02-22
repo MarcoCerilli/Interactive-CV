c3ec1fac8f65934448a8756ff6f808404fbf0ba4
Marco Cerilli <cerillimarco15@gmail.com>
2025-02-22 16:09:35 +0100

    Aggiornato il file del CV

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("darkModeBtn").addEventListener("click", function() {
 
 document.getElementById("downloadCV").addEventListener("click", function() {
     const link = document.createElement("a");
-    link.href = "https://marcocerilli.github.io/Interactive-CV/";
+    link.href = "https://marcocerilli.github.io/Interactive-CV/CV_Marco_Cerilli.pdf";
     link.download = "CV_Marco_Cerilli";
     document.body.appendChild(link);
     link.click();
